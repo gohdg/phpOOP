84792a67d57f5016e0588b98a37648eeb7cabb0c
gohdg <gohdg@naver.com>
2021-06-10 18:04:10 +0900

    modified autoloder in case the file needs to call class from not the root folder, and type casting

---
+++ includes/class-autoload.inc.php
@@ -0,0 +1,36 @@
+<?php
+// spl_autoload_register("myAutoLoader");
+
+// function myAutoLoader($className)
+// {
+//   $path = "classes/";
+//   $extension = ".class.php";
+//   $fullpath = $path . $className . $extension;
+
+//   if (!file_exists($fullpath)) {
+//     return false;
+//   }
+
+//   require_once $fullpath;
+// }
+
+spl_autoload_register("myAutoLoader");
+function myAutoLoader($className)
+{
+  $url = $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"];
+
+  if (strpos($url, "includes") !== false) {
+    $path = "../classes/";
+  } else {
+    $path = "classes/";
+  }
+
+  $extension = ".class.php";
+  $fullpath = $path . $className . $extension;
+
+  if (!file_exists($fullpath)) {
+    return false;
+  }
+
+  require_once $fullpath;
+}
--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
-// declare(strict_types=1);
-include "includes/autoloader.inc.php"; ?>
+declare(strict_types=1);
+include "includes/class-autoload.inc.php";
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -11,15 +12,19 @@ include "includes/autoloader.inc.php"; ?>
     <title>Document</title>
 </head>
 <body>
-    <?php
-    $person1 = new Person();
+    <form action="includes/calc.inc.php" method="post">
+        <p>My own calculator!</p>
+        <input type="number" name="num1" placeholder="First number">
+        <select name="oper">
+            <option value="add">Addition</option>
+            <option value="sub">Subtraction</option>
+            <option value="div">Division</option>
+            <option value="mul">Multiplication</option>
+        </select>
+        <input type="number" name="num2" placeholder="Second number">
+        <button type="submit" name="submit">Calculate</button>
 
-    try {
-      $person1->setName(2);
-      echo $person1->getName();
-    } catch (TypeError $e) {
-      echo "Error!: " . $e->getMessage();
-    }
-    ?>
+    </form>
+    
 </body>
 </html>
