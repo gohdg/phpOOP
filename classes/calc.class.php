8854ea144280406dec525a30a44824f85e1760b9
gohdg <gohdg@naver.com>
2021-06-10 18:35:37 +0900

    how to access the CONSTANT and static variables from the static functoin which is inherited. when test, run calc.class.php instead of index.php in this lecture

--- classes/calc.class.php
+++ classes/calc.class.php
@@ -1,37 +1,30 @@
 <?php
-class Calc
+class FirstClass
 {
-  public $num1;
-  public $num2;
-  public $operator;
+  const EXAMPLE = "You can't change this!";
 
-  public function __construct(string $operator, int $num1, int $num2)
+  public static function test()
   {
-    $this->operator = $operator;
-    $this->num1 = $num1;
-    $this->num2 = $num2;
-  }
-
-  public function calculator()
-  {
-    switch ($this->operator) {
-      case "add":
-        $result = $this->num1 + $this->num2;
-        break;
-      case "sub":
-        $result = $this->num1 - $this->num2;
-        break;
-      case "div":
-        $result = $this->num1 / $this->num2;
-        break;
-      case "mul":
-        $result = $this->num1 * $this->num2;
-        break;
-      default:
-        echo "Error!";
-
-        break;
-    }
-    return $result;
+    $testing = "This is a test!";
+    return $testing;
   }
 }
+
+// $a = FirstClass::EXAMPLE;
+// echo $a;
+// echo "<br>";
+// echo FirstClass::test();
+
+class SecondClass extends FirstClass
+{
+  public static $staticProperty = "A static property!";
+
+  public static function anotherTest()
+  {
+    echo parent::EXAMPLE;
+    echo self::$staticProperty;
+  }
+}
+
+$b = SecondClass::anotherTest();
+echo $b;
